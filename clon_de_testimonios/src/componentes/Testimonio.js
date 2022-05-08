c88d49d7c47c6419bfc8e83c149179abff643da0
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2022-05-08 18:17:23 -0300

    completo los datos de las props para poder tener tres testimonios distintos

--- clon_de_testimonios/src/componentes/Testimonio.js
+++ clon_de_testimonios/src/componentes/Testimonio.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../stylesheets/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
   return(
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio'
-        src={require('../imagenes/testimonio-emma.png')}
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
         alt='Foto de Emma'/> 
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostina en Suecia</p>
-        <p className='cargo-testimonio'>Ingeniera de Sorftware en Spotify</p>
-        <p className='texti-testimonio'>I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.</p>
+        <p className='nombre-testimonio'>{props.nombre} in {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} at {props.empresa}</p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
       </div>
     </div>
   );
